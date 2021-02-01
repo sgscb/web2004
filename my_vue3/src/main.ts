c7d394c193080471e331749fbd91de857d836240
bing <bing@123.com>
2021-02-02 00:52:59 +0800

    feat:zhiye

--- my_vue3/src/main.ts
+++ my_vue3/src/main.ts
@@ -4,12 +4,12 @@ import App from './App.vue'
 const app = createApp(App)
 // 这个时候 app 就是一个 App 的实例，现在再设置任何的配置是在不同的 app 实例上面的，不会像vue2 一样发生任何的冲突。
 
-app.config.isCustomElement = tag => tag.startsWith('app-') // ？----
+// app.config.isCustomElement = tag => tag.startsWith('app-') // ？----
 // app.use(/* ... */)
 // app.mixin(/* ... */)
 // app.component(/* ... */)
 // app.directive(/* ... */)
 
-app.config.globalProperties.customProperty = () => { }
+// app.config.globalProperties.customProperty = () => { }
 
 createApp(App).mount('#app')
