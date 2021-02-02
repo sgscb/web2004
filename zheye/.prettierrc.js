4e62618282241a2d56df203a29b593b509542ba1
bing <bing@123.com>
2021-02-03 01:04:36 +0800

    feat:vue3ClumnLiset

--- zheye/.prettierrc.js
+++ zheye/.prettierrc.js
@@ -1,5 +1,6 @@
 module.exports = {
+  eslintIntegration: true, //与eslint整合
   singleQuote: true, // true表示开启强制使用单引号
-  semi: false, // 结尾是否使用分号
+  // semi: false, // 结尾是否使用分号
   trailingComma: 'none' // 对象最后一项默认格式化会加逗号, 这里取消
-}
+};
